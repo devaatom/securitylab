d8006994485943671f7e8ac3ad2f8a521bf2bd07
sashanksridhar <sashank.ssridhar@gmail.com>
2019-09-16 14:09:12 +0530

    Lab8

---
+++ Lab8/sha1.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class sha1{
+	public static void main(String a[])
+	{
+		try{
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Input");
+			String s = sc.nextLine();
+			byte[] digest = md.digest(s.getBytes());
+			BigInteger no = new BigInteger(1,digest);
+			String hashtext = no.toString(16);
+			while(hashtext.length()<32){
+			hashtext = "0"+hashtext;
+			}
+			System.out.println(hashtext);
+		}
+		catch(NoSuchAlgorithmException e)
+		{
+			System.out.println(e);
+		}
+		}
+	}
